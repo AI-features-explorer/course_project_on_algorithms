c28e6be70e2d242b6dbfe2f99ad41565c434896e
User_Name <docf37@gmail.com>
2023-12-20 12:27:30 +0300

    add the basic console ui

--- Source code/main.cpp
+++ Source code/main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstdlib> // Для функции rand()
+#include <ctime>   // Для функции time()
 using namespace CourseProjectOnAlgorithms;
 using namespace std;
 
@@ -11,25 +13,29 @@ int main()
     BinaryTree* tree = new BinaryTree(20);
 
     // Insert elements into the tree
-    tree->insert(10);
-    tree->insert(30);
-    tree->insert(5);
-    tree->insert(15);
-    tree->insert(25);
-    tree->insert(35);
+    srand(time(nullptr)); 
+        for (int i = 0; i < 14; ++i) {
+            tree->insert(rand() % 50); // Вставляем случайное значение от 0 до 50
+        }
 
     // Print the tree
     cout << "Binary Tree:" << std::endl;
     tree->printTree();
 
-    // Find Lowest Common Ancestor (LCA) of two nodes
-    
-    cout << "LCA is "<< tree->findLCA(tree, 5, 15);
+    // geting the variabels
+    int x; int y;
+    cout << "Write the first hode: ";
+    cin >> x;
+    cout << "Write the first hode: ";
+    cin >> y;
 
-    // Clean up the memory
+    // Find Lowest Common Ancestor (LCA) of two nodes
+    cout << "LCA is "<< tree->findLCA(tree, x, y);
+
+    // Clean up the memory and wait 
     delete tree;
-    int a;
-    cin >> a;
+    int end;
+    cin >> end;
     
     return 0;
 }
